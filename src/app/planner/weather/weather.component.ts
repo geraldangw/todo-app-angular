3cdb5985fe821b74bb0e9cc669f29ddf1e344ed8
Gerald Ang <geraldangw@gmail.com>
2016-09-05 21:46:30 +0800

    done w weather api

--- src/app/planner/weather/weather.component.ts
+++ src/app/planner/weather/weather.component.ts
@@ -21,13 +21,13 @@ constructor(public http: Http) {
 
   makeRequest(): void {
     this.loading = true;
-    this.http.get('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22singapore%2Csg%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys')
+    this.http.request('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22singapore%2Csg%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys')
     .subscribe((res: Response) => {
             this.country = res.json().query.results.channel.location.city.toUpperCase();
             this.temperature = res.json().query.results.channel.item.condition.temp;
             this.forecast = res.json().query.results.channel.item.condition.text;
-            this.mintemp = res.json().query.results.channel.forecast.low;
-            this.maxtemp = res.json().query.results.channel.forecast.high;
+            this.mintemp = res.json().query.results.channel.item.forecast[0].low;
+            this.maxtemp = res.json().query.results.channel.item.forecast[0].high;
             this.loading = false;
           });
 
